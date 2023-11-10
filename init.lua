4ee575a57b6e896b5a7522779b7194b962866d8a
Joe Diragi <joseph.diragi@icloud.com>
2023-11-10 18:58:32 -0500

    Add elixir lsp

--- init.lua
+++ init.lua
@@ -313,6 +313,29 @@ require('telescope').setup {
   },
 }
 
+local on_attach_exlsp = function(client, bufnr)
+  local opts = { noremap=true, silent=true }
+ 
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cd', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+end
+
+require('lspconfig').elixirls.setup {
+  cmd = { "$HOME/.elixir/elixir-ls/language_server.sh" },
+  on_attach = on_attach_exlsp,
+  capabilities = require('cmp_nvim_lsp').default_capabilities()
+}
+
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
@@ -597,6 +620,16 @@ cmp.setup {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
   },
+}
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {"elixir", "heex", "eex"},
+  sync_install = false,
+  ignore_install = { },
+  highlight = {
+    enable = true,
+    disable = { },
+  }
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
